83b92c2c7179988e1cf4b8eb769c6757f4c09b91
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-06-29 16:08:12 -0300

    modified DeleteRoutine

--- src/modules/workout/deleteRoutine/DeleteRoutinesController.ts
+++ src/modules/workout/deleteRoutine/DeleteRoutinesController.ts
@@ -1,21 +1,18 @@
 import { Request, Response } from 'express';
 import { DeleteRoutinesUseCase } from './DeleteRoutinesUseCase';
 
-
 export class DeleteRoutinesController {
   async handle(request: Request, response: Response) {
     try {
       const deleteRoutinesUseCase = new DeleteRoutinesUseCase();
-
       const id = request.params.id;
-
-
       const result = await deleteRoutinesUseCase.delete(id);
 
       return response.sendStatus(204);
     } catch (error) {
-      console.log(error);
-      return response.status(400).json({ message: `Error deleting user: ${error.message}` });
+      // console.log(error);
+      const errorMessage = error.message; // Captura a mensagem de erro
+      return response.status(400).json({ message: `Error deleting routine: ${errorMessage}` });
     }
   }
 }
--- src/modules/workout/deleteRoutine/DeleteRoutinesUseCase.ts
+++ src/modules/workout/deleteRoutine/DeleteRoutinesUseCase.ts
@@ -3,14 +3,26 @@ import { prisma } from "../../../database/prismaClient";
 interface IDeleteRoutinesUseCase {
   id: string;
 }
+
 export class DeleteRoutinesUseCase {
   async delete(id: string) {
-    console.log('id do exercidios backend', id)
-    const trainings = await prisma.routine.delete({
-      where: {
-        id,
-      },
-    });
-    return trainings;
+    try {
+      const routines = await prisma.routine.delete({
+        where: {
+          id,
+        },
+      });
+      return routines;
+    } catch (error) {
+      if (error.code === 'P2003' && error.meta?.field_name === 'routineId') {
+        // Retorne a mensagem de erro personalizada
+        const errorMessage = 'Erro ao excluir a rotina: a restrição de chave estrangeira falhou.';
+        console.error(errorMessage);
+        return { error: errorMessage };
+      }
+      // Trate outros erros aqui, se necessário
+      console.error("Ocorreu um erro ao excluir as rotinas:", error);
+      throw error;
+    }
   }
 }
